f59254aa10489f1781df0c5aa518bfbb7f407094
Jiahao <qaz7416@gmail.com>
2025-06-16 09:14:14 +0800

    1

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,31 +1,11 @@
-import React, { useState } from 'react';
-import Sidebar from '../components/Sidebar';
-import ChannelList from '../components/ChannelList';
-import Topbar from '../components/Topbar';
-import PostList from '../components/PostList';
-import PostDetail from '../components/PostDetail';
-import MessageInput from '../components/MessageInput';
+import type { NextPage } from 'next'
 
-export default function Home() {
-  const [selectedPostId, setSelectedPostId] = useState<string | null>(null);
-
+const Home: NextPage = () => {
   return (
-    <div className="flex h-screen bg-gray-900">
-      <Sidebar />
-      <ChannelList />
-      <div className="flex flex-col flex-1 min-w-0">
-        <Topbar />
-        <div className="flex flex-1 min-h-0">
-          <PostList
-            selectedPostId={selectedPostId}
-            onSelect={(id: string) => setSelectedPostId(prev => prev === id ? null : id)}
-          />
-          {selectedPostId && (
-            <PostDetail postId={selectedPostId} />
-          )}
-        </div>
-        <MessageInput />
-      </div>
+    <div className="min-h-screen flex items-center justify-center">
+      <h1 className="text-4xl font-bold">Hello World</h1>
     </div>
-  );
+  )
 }
+
+export default Home
